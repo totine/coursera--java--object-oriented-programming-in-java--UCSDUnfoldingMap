deb73a07bd97381f8989c6fa827261604edd9898
Joanna <joanna.gargas@gmail.com>
2018-01-21 16:24:47 +0100

    Add solution for Assignment for module 1

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -76,7 +76,9 @@ public class HelloWorld extends PApplet
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
-
+		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+		map2.zoomAndPanTo(zoomLevel, new Location(49.98f, 20.059f));
+		MapUtils.createDefaultEventDispatcher(this, map2);
 	}
 
 	/** Draw the Applet window.  */
@@ -84,7 +86,9 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
 }
+
